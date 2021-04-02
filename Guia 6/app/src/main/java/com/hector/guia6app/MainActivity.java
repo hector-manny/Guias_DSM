f66461065bf4e163b0420b62a5e856fc5c27bfe0
hector martinez <mejia.martinez2012@hotmail.es>
2021-04-02 00:28:54 -0600

    Guia 6 app frame

---
+++ Guia 6/app/src/main/java/com/hector/guia6app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hector.guia6app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo_frame_layout);
+    }
+}
