6c57e57f0da177db18f22564d07b1271577e297c
hector martinez <mejia.martinez2012@hotmail.es>
2021-04-26 22:06:58 -0600

    up guias

--- Guia 7/app/src/main/java/com/hector/guia7/MainActivity.java
+++ Guia 7/app/src/main/java/com/hector/guia7/MainActivity.java
@@ -1,14 +1,38 @@
 package com.hector.guia7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button registerBtn, loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+    }
+    }
