51f1961c62d54967c96bf455ba7f5563e8bc209c
hector martinez <mejia.martinez2012@hotmail.es>
2021-05-08 22:02:15 -0600

    Guia 10

---
+++ Guia 10/app/src/main/java/com/hector/guia10/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hector.guia10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
